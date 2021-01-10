267ef3ee6baa3ea59c24b5472f4bb6a31f865682
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2021-01-10 00:27:03 -0300

    Aplicando estilos globais

--- src/assets/globalStyle.tsx
+++ src/assets/globalStyle.tsx
@@ -1,10 +1,13 @@
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    *{
+  *{
     margin: 0;
     padding: 0;
     outline: none;
+  }
+  p,h1,h2{
+    font-family: 'Bodoni Moda', serif;
   }
 `;
 
