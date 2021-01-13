562fe9ff5f93a8cdda31fb42e0417e29278f37ef
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2021-01-12 21:28:50 -0300

    Criando fonte em <a/> e <button/>

--- src/assets/globalStyle.tsx
+++ src/assets/globalStyle.tsx
@@ -9,8 +9,12 @@ const GlobalStyle = createGlobalStyle`
   button,a{
     cursor:pointer;
   }
-  p,h1,h2{
+  p,h1,h2,a,button{
     font-family: 'Bodoni Moda', serif;
+  }
+  a,button{
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight:400;
   }
   h1{
     font-weight:200;
