1bbab47ca2a18529f4786f11df1c310ecc3081f1
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2021-01-12 21:33:54 -0300

    Criando estrutura em src/components/explore

---
+++ src/components/explore/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {} from 'framer-motion';
+// import { Container } from './styles';
+
+const explore: React.FC = () => {
+  return (
+      <>
+        <div className=''>
+            <p className=''></p>
+            <p className=''></p>
+        </div>
+        <div className=''>
+
+        </div>
+      </>
+  );
+}
+
+export default explore;
---
+++ src/components/explore/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const exploreDiv = styled.div`
+    display:flex;
+`;
+
+export const exploreDivInfo =  styled.div`
+    
+`;
+
+export const exploreDivImage =  styled.div`
+
+`;
+
