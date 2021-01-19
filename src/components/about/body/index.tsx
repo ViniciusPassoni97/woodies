5507b057a8b6e48a72eb1a41283206ed6276121b
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-01-18 21:31:53 -0300

    Adicionando ultimos componentes estruturais

--- src/components/about/body/index.tsx
+++ src/components/about/body/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import {ContainerAboutBody,ContainerAboutBodyImage,ContainerAboutBodyInfo} from './style';
+import AboutImage from '../../../assets/icons/aboutImage.png';
 
 const Body: React.FC = () => {
     return (
         <ContainerAboutBody>
             <ContainerAboutBodyImage>
-                <img src='' alt='' className='' />
+                <img src={AboutImage} alt='ContainerAboutBodyImg' className='ContainerAboutBodyImg' />
             </ContainerAboutBodyImage>
             <ContainerAboutBodyInfo>
-                <p className=''></p>
+                <p className='ContainerAboutBodyInfoTitle'>WOODIES is the home of modern wooden furniture</p>
+                <p className='ContainerAboutBodyInfoText'>the answer to your need for furniture with shapes,
+                 sizes and colors.</p>
             </ContainerAboutBodyInfo>
         </ContainerAboutBody>
     );
