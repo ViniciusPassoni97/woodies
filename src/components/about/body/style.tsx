7072d001c5c81014f708744318fb9aec1b1a5574
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-01-19 22:22:13 -0300

    Estilos em About/Body

--- src/components/about/body/style.tsx
+++ src/components/about/body/style.tsx
@@ -3,13 +3,26 @@ import styled from 'styled-components';
 export const ContainerAboutBody = styled.div`
     display:flex;
     width:80%;
+    height:25rem;
     margin:auto;
 `;
 
 export const ContainerAboutBodyImage = styled.div`
-
+    margin-top:5rem;
+    margin-left:5rem;
+    .ContainerAboutBodyImg{
+        width:16rem;
+        height:16rem;
+    }
 `;
 export const ContainerAboutBodyInfo = styled.div`
     margin:auto;
     margin-left:5rem;
+    .ContainerAboutBodyInfoTitle{
+        font-size:2.5rem;
+    }
+    .ContainerAboutBodyInfoText{
+        margin-top:0.5rem;
+        font-size:1.8rem;
+    }
 `;
